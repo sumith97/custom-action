94e9e3f49c55bcf6e004600b6445440784fff660
sumithtest1997 <162404732+sumithtest1997@users.noreply.github.com>
2024-08-29 13:57:52 -0400

    small

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const test = require('./test');
-
+const fs = require('fs');
 async function run() {
   try {
     // Get the input named 'who-to-greet'
@@ -20,6 +20,8 @@ async function run() {
     // Log the greeting
     console.log(`Hello ${nameToGreet} at ${time} yo are crazy fellow! and ${fun}`);
     console.log('Bro you are crazy there is still lot to learn in github actions. And I love this shit');
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, `time=${time}\n`);
+fs.appendFileSync(process.env.GITHUB_OUTPUT, `fun=${fun}\n`);
   } catch (error) {
     core.setFailed(error.message);
   }
